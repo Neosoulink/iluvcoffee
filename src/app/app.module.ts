02041f8b1f1d077ad6510763fef0624da5ec88b3
Nathan Mande <neodarksoulink@gmail.com>
2024-03-08 08:12:53 +0200

    feat: init coffees controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
+
+import { AppController } from './app.controller';
+import { CoffeesController } from '../coffees/coffees.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CoffeesController],
   providers: [AppService],
 })
 export class AppModule {}
