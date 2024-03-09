a05a3251caf2a1b859a1e5274547edbb3dd14c8a
Nathan Mande <neodarksoulink@gmail.com>
2024-03-09 03:51:43 +0200

    chore: implement typeorm module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { CoffeesModule } from 'src/coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'iluvcoffee-db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
