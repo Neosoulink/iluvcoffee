4bea776276d8c95e8ec05b3a89a95cb24449087f
Nathan Mande <neodarksoulink@gmail.com>
2024-03-10 08:48:10 +0200

    feat: secure env variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from '@hapi/joi';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { CoffeesModule } from 'src/coffees/coffees.module';
 import { CoffeeRatingModule } from 'src/coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DB_HOST: Joi.required(),
+        DB_PORT: Joi.number().default(5432),
+        DB_USER: Joi.required(),
+        DB_PASS: Joi.required(),
+        DB_NAME: Joi.required(),
+      }),
+    }),
     CoffeesModule,
     CoffeeRatingModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'root',
-      password: 'root',
-      database: 'iluvcoffee-db',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
