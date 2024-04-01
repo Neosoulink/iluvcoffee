423cd74c98e68b8b7d6c545663099bf1a1914831
Nathan Mande <neodarksoulink@gmail.com>
2024-04-01 10:26:30 +0200

    refactor: add DTO coffee description

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'Coffee name.' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: 'Coffee brand' })
   @IsString()
   readonly brand: string;
 
+  @ApiProperty({ example: [] })
   @IsString({ each: true })
   readonly flavors: string[];
 }
