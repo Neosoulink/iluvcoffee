40b311809bde6419b3f7702bfd516fe3c6a52a7c
Nathan Mande <neodarksoulink@gmail.com>
2024-04-14 07:55:58 +0200

    feat: implement base e2e coffee test

---
+++ test/coffee/coffees.e2e-spec.ts
@@ -0,0 +1,42 @@
+import { INestApplication } from '@nestjs/common';
+import { TestingModule, Test } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { CoffeesModule } from '../../src/coffees/coffees.module';
+
+console.log(process.env);
+
+describe('[Feature] Coffees - /coffees', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        CoffeesModule,
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5434,
+          username: 'root',
+          password: 'root',
+          database: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it.todo('Create [POST /]');
+  it.todo('Get all [GET /]');
+  it.todo('Get one [GET /:id]');
+  it.todo('Update one [PATCH /:id]');
+  it.todo('Delete one [DELETE /:id]');
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
