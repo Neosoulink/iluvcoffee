7eb37a7e2bf9962f7f981123bf41461c41541ef7
Nathan Mande <neodarksoulink@gmail.com>
2024-04-19 00:06:44 +0200

    feat: add `mongo-db` connection support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import * as Joi from '@hapi/joi';
 
 import { CoffeesModule } from '../coffees/coffees.module';
@@ -13,29 +14,54 @@ import { AppController } from './app.controller';
 
 import appConfig from '../config/app.config';
 
+type JoiValidationSchema =
+  | {
+      DB_HOST: Joi.StringSchema;
+      DB_PORT: Joi.NumberSchema;
+      DB_NAME: Joi.StringSchema;
+      DB: Joi.StringSchema;
+    }
+  | {
+      DB_HOST: Joi.StringSchema;
+      DB_PORT: Joi.NumberSchema;
+      DB_USER: Joi.StringSchema;
+      DB_PASS: Joi.StringSchema;
+      DB_NAME: Joi.StringSchema;
+      DB: Joi.StringSchema;
+    };
+
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
-        type: 'postgres',
         host: process.env.DB_HOST,
         port: +process.env.DB_PORT,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
         database: process.env.DB_NAME,
-        autoLoadEntities: true,
         synchronize: true,
+        ...(process.env.DB === 'mongo'
+          ? {
+              type: 'mongodb',
+              namingStrategy: new SnakeNamingStrategy(),
+            }
+          : {
+              type: 'postgres',
+              autoLoadEntities: true,
+              username: process.env.DB_USER,
+              password: process.env.DB_PASS,
+            }),
       }),
     }),
     ConfigModule.forRoot({
       load: [appConfig],
       validationSchema: Joi.object({
-        DB_HOST: Joi.required(),
+        DB: Joi.string().valid('mongo', 'postgres'),
+
+        DB_HOST: Joi.string().required(),
         DB_PORT: Joi.number().default(5432),
-        DB_USER: Joi.required(),
-        DB_PASS: Joi.required(),
-        DB_NAME: Joi.required(),
-      }),
+        DB_NAME: Joi.string().required(),
+        DB_USER: Joi.string(),
+        DB_PASS: Joi.string(),
+      }) satisfies Joi.ObjectSchema<JoiValidationSchema>,
     }),
     CoffeesModule,
     CoffeeRatingModule,
